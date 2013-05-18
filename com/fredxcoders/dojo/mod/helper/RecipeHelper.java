4f36918e0bb8a45410c56b59ce52c3eb94ec29f1
Elliott Sperlazza <esperlaz@localhost.localdomain>
2013-05-18 14:47:08 -0400

    Added getter for recipe

--- com/fredxcoders/dojo/mod/helper/RecipeHelper.java
+++ com/fredxcoders/dojo/mod/helper/RecipeHelper.java
@@ -32,26 +32,35 @@ public class RecipeHelper {
 		String row2 = ((input[1][0] != null) ? "d" : " ") + ((input[1][1] != null) ? "e" : " ") + ((input[1][2] != null) ? "f" : " ");
 		String row3 = ((input[2][0] != null) ? "g" : " ") + ((input[2][1] != null) ? "h" : " ") + ((input[2][2] != null) ? "i" : " ");
 		
-		Object[] out = new Object[2 + 18 - (2 * nullCount)];
+		Object[] recipe = new Object[2 + 18 - (2 * nullCount)];
 		char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
 		
 		//Does this: new Object[] { "DDD", "DDD", "DDD",
-		out[0] = row1;
-		out[1] = row2;
-		out[2] = row3;
+		recipe[0] = row1;
+		recipe[1] = row2;
+		recipe[2] = row3;
 		
 		int letterCount = 0;
 		for(int x=0; x<3; x++){
 			for(int y=0; y<3; y++){
 				if(input[x][y] == null){
 					//does this: 'D', Block.dirt})
-					out[2*letterCount +3] = letters[letterCount];
-					out[2*letterCount+4] = input[x][y];
+					recipe[2*letterCount +3] = letters[letterCount];
+					recipe[2*letterCount+4] = input[x][y];
 				}
 				letterCount++;
 			}
 		}
-		ModLoader.addRecipe(output, out);
+		ModLoader.addRecipe(output, recipe);
+	}
+	
+	/**
+	 * Gets an empty 3x3 ItemStack array to be used for recipes
+	 * @return
+	 */
+	public static ItemStack[][] getBlankRecipe(){
+		ItemStack[][] recipe = new ItemStack[3][3];
+		return recipe;
 	}
 	
 }
