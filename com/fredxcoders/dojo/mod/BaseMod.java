93644f4897655efdbafd4b414cf5d771b316c10c
kfp <dev@sperlazzasaurus.com>
2013-07-17 15:52:18 -0400

    added new block

--- com/fredxcoders/dojo/mod/BaseMod.java
+++ com/fredxcoders/dojo/mod/BaseMod.java
@@ -2,9 +2,12 @@ package com.fredxcoders.dojo.mod;
 
 
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 
+import com.fredxcoders.dojo.mod.blocks.MyBlock;
 import com.fredxcoders.dojo.mod.helper.RecipeHelper;
 
 import cpw.mods.fml.common.Mod;
@@ -12,6 +15,8 @@ import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid="dojo_BaseMod", name="Base Mod", version="0.1")
 @NetworkMod(clientSideRequired=true,serverSideRequired=true)
@@ -19,11 +24,24 @@ public class BaseMod {
 
 	@Instance("dojo_BaseMod")
 	public static BaseMod instance;
+	
+	public final static Block myBlock = new MyBlock(500, Material.ground)
+				.setHardness(0.5F).setStepSound(Block.soundMetalFootstep)
+			    .setUnlocalizedName("myBlock").setCreativeTab(CreativeTabs.tabBlock)
+			    .setLightValue(0.0F)
+			    .setLightOpacity(0);
 
 	@Init
 	public void load(FMLInitializationEvent event){
+		
+		LanguageRegistry.addName(myBlock, "My Block");
+        MinecraftForge.setBlockHarvestLevel(myBlock, "shovel", 0);
+        GameRegistry.registerBlock(myBlock, "myBlock");
+		
 		ItemStack[][] recipe = RecipeHelper.getBlankRecipe();
 		recipe[0][0] = new ItemStack(Block.dirt);
-		RecipeHelper.addRecipe(new ItemStack(Block.dirt), recipe);	//dirt makes dirt!
+		RecipeHelper.addRecipe(new ItemStack(myBlock), recipe);	//dirt makes dirt!
+		
+//		proxy.registerRenderers();	
 	}
 }
---
+++ com/fredxcoders/dojo/mod/blocks/MyBlock.java
@@ -0,0 +1,19 @@
+package com.fredxcoders.dojo.mod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class MyBlock extends Block {
+
+	public MyBlock(int id, Material material) {
+		super(id, material);
+		// TODO Auto-generated constructor stub
+	}
+
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
+        this.blockIcon = iconRegister.registerIcon("BaseMod:test");
+    }
+	
+}
